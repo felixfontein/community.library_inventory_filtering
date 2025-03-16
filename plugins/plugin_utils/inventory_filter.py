930c8d2c354157c7065b99e2e042cd6b8810bc0d
Felix Fontein <felix@fontein.de>
2025-03-16 18:57:45 +0100

    Reformat with black.

--- plugins/plugin_utils/inventory_filter.py
+++ plugins/plugin_utils/inventory_filter.py
@@ -4,7 +4,9 @@
 # GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from __future__ import (absolute_import, division, print_function)
+from __future__ import absolute_import, division, print_function
+
+
 __metaclass__ = type
 
 from ansible.errors import AnsibleError, AnsibleParserError
@@ -14,7 +16,7 @@ from ansible.module_utils.parsing.convert_bool import boolean
 from ansible.module_utils.six import string_types
 
 
-_ALLOWED_KEYS = ('include', 'exclude')
+_ALLOWED_KEYS = ("include", "exclude")
 
 
 def parse_filters(filters):
@@ -26,26 +28,36 @@ def parse_filters(filters):
         return result
     for index, filter in enumerate(filters):
         if not isinstance(filter, Mapping):
-            raise AnsibleError('filter[{index}] must be a dictionary'.format(
-                index=index + 1,
-            ))
+            raise AnsibleError(
+                "filter[{index}] must be a dictionary".format(
+                    index=index + 1,
+                )
+            )
         if len(filter) != 1:
-            raise AnsibleError('filter[{index}] must have exactly one key-value pair'.format(
-                index=index + 1,
-            ))
+            raise AnsibleError(
+                "filter[{index}] must have exactly one key-value pair".format(
+                    index=index + 1,
+                )
+            )
         key, value = list(filter.items())[0]
         if key not in _ALLOWED_KEYS:
-            raise AnsibleError('filter[{index}] must have a {allowed_keys} key, not "{key}"'.format(
-                index=index + 1,
-                key=key,
-                allowed_keys=' or '.join('"{key}"'.format(key=key) for key in _ALLOWED_KEYS),
-            ))
+            raise AnsibleError(
+                'filter[{index}] must have a {allowed_keys} key, not "{key}"'.format(
+                    index=index + 1,
+                    key=key,
+                    allowed_keys=" or ".join(
+                        '"{key}"'.format(key=key) for key in _ALLOWED_KEYS
+                    ),
+                )
+            )
         if not isinstance(value, (string_types, bool)):
-            raise AnsibleError('filter[{index}].{key} must be a string, not {value_type}'.format(
-                index=index + 1,
-                key=key,
-                value_type=type(value),
-            ))
+            raise AnsibleError(
+                "filter[{index}].{key} must be a string, not {value_type}".format(
+                    index=index + 1,
+                    key=key,
+                    value_type=type(value),
+                )
+            )
         result.append(filter)
     return result
 
@@ -55,7 +67,7 @@ def filter_host(inventory_plugin, host, host_vars, filters):
     Determine whether a host should be accepted (``True``) or not (``False``).
     """
     vars = {
-        'inventory_hostname': host,
+        "inventory_hostname": host,
     }
     if host_vars:
         vars.update(host_vars)
@@ -70,21 +82,23 @@ def filter_host(inventory_plugin, host, host_vars, filters):
             templar.available_variables = vars
             return boolean(templar.template(conditional))
         except Exception as e:
-            raise AnsibleParserError("Could not evaluate filter condition {condition!r} for host {host}: {err}".format(
-                host=host,
-                condition=condition,
-                err=to_native(e),
-            ))
+            raise AnsibleParserError(
+                "Could not evaluate filter condition {condition!r} for host {host}: {err}".format(
+                    host=host,
+                    condition=condition,
+                    err=to_native(e),
+                )
+            )
         finally:
             templar.available_variables = old_vars
 
     for filter in filters:
-        if 'include' in filter:
-            expr = filter['include']
+        if "include" in filter:
+            expr = filter["include"]
             if evaluate(expr):
                 return True
-        if 'exclude' in filter:
-            expr = filter['exclude']
+        if "exclude" in filter:
+            expr = filter["exclude"]
             if evaluate(expr):
                 return False
 
