69fc2f43ee953b33cbaac37c6f1cd9315b566cce
Felix Fontein <felix@fontein.de>
2025-04-18 13:11:18 +0200

    Use antsibull-nox.toml instead of defining everying in noxfile. (#34)

--- noxfile.py
+++ noxfile.py
@@ -18,44 +18,7 @@ except ImportError:
     sys.exit(1)
 
 
-antsibull_nox.setup(
-    collection_sources={
-        "community.internal_test_tools": "git+https://github.com/ansible-collections/community.internal_test_tools.git,main",
-    },
-)
-
-
-antsibull_nox.add_lint_sessions(
-    isort_config="tests/nox-config-isort.cfg",
-    black_config="tests/nox-config-black.toml",
-    flake8_config="tests/nox-config-flake8.ini",
-    pylint_rcfile="tests/nox-config-pylint.rc",
-    run_yamllint=True,
-    yamllint_config="tests/nox-config-yamllint.yml",
-    mypy_config="tests/nox-config-mypy.ini",
-    mypy_extra_deps=[
-        "types-mock",
-    ],
-)
-
-antsibull_nox.add_docs_check(
-    validate_collection_refs="all",
-)
-
-antsibull_nox.add_license_check()
-
-antsibull_nox.add_extra_checks(
-    run_no_unwanted_files=True,
-    no_unwanted_files_module_extensions=[".py"],
-    no_unwanted_files_yaml_extensions=[".yml"],
-    no_unwanted_files_skip_paths=[
-        "plugins/plugin_utils/py.typed",
-    ],
-)
-
-antsibull_nox.add_build_import_check(
-    run_galaxy_importer=True,
-)
+antsibull_nox.load_antsibull_nox_toml()
 
 
 # Allow to run the noxfile with `python noxfile.py`, `pipx run noxfile.py`, or similar.
