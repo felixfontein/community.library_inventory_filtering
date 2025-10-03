7862929738e1a1ca563dffb546354a82e4cf56b8
Felix Fontein <felix@fontein.de>
2025-10-03 20:27:00 +0200

    Avoid six in favor of ugly ifs. (#39)

--- plugins/plugin_utils/inventory_filter.py
+++ plugins/plugin_utils/inventory_filter.py
@@ -9,11 +9,17 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
+import sys
+
 from ansible.errors import AnsibleError, AnsibleParserError
 from ansible.module_utils.common.text.converters import to_native
 from ansible.module_utils.parsing.convert_bool import boolean
-from ansible.module_utils.six import string_types
 
+
+if sys.version_info[0] == 2:
+    string_types = (str, unicode)  # noqa: F821, pylint: disable=undefined-variable
+else:
+    string_types = (bytes, str)
 
 try:
     from collections.abc import Mapping
